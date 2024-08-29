c6c8ae0f781d92efc343d06487efd44573fe2f2e
Jose Portillo <joseportillo2002.jdpf@gmail.com>
2024-08-29 18:49:23 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavBar } from "./Components/NavBar";
+import { Footer } from "./Components/Footer";
+import routes from "./Config/routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Route
+          path="/"
+          render={({ location }) => {
+            const isLoginRoute =
+              location.pathname === "/Login" ||
+              location.pathname === "/Dashboard" ||
+              location.pathname === "/Register" ||
+              location.pathname === "/Recover" ||
+              location.pathname === "/RecoverUpdate" ||
+              location.pathname === "/AdmEc";
+            return (
+              <>
+                {!isLoginRoute && <NavBar />}
+                <Switch>
+                  {routes.map((route) => (
+                    <Route
+                      key={route.path}
+                      path={route.path}
+                      component={route.component}
+                    />
+                  ))}
+                </Switch>
+                {!isLoginRoute && <Footer />}
+              </>
+            );
+          }}
+        />
+      </Router>
+    </React.Fragment>
   );
 }
 
